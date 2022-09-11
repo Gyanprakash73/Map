e72f3ad062196482ede8a7385ae68c41db93d8b9
Gyan Prakash <princekumar3510@gmail.com>
2022-09-11 16:49:02 +0530

    File Formating and code formating

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import Navigation from './src/Navigation';
+import Navigation from './src/StackNavigation/Navigation';
 
 const App = () => {
   return <Navigation />;
